b19255986edefcf52f2865074b55ba1805d40d2b
WNjihia <dorisn172@gmail.com>
2019-02-06 20:39:42 +0300

    remove tweet-deleted screenshot

--- cypress/integration/helpers/Authentication.js
+++ cypress/integration/helpers/Authentication.js
@@ -31,7 +31,6 @@ class Auth{
   }
 
   logout(){
-    cy.screenshot('tweet-deleted');
     cy.get('#user-dropdown-toggle').click();
     cy.get('.js-signout-button > .dropdown-link').click({force: true});
 
